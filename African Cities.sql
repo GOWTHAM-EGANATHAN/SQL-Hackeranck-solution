0aa4e8dcbea7f0bdc2aa3ee21795724cf21faaf0
Gowtham Eganathan <51506726+GOWTHAM-EGANATHAN@users.noreply.github.com>
2021-10-23 00:04:10 +0530

    Update African Cities.sql

--- African Cities.sql
+++ African Cities.sql
@@ -1,1 +1,5 @@
-select city.name from city join country on city.countrycode = country.code where country.continent = 'Africa';
+select city.name 
+from city 
+join country 
+on city.countrycode = country.code 
+where country.continent = 'Africa';
